6671a49e8f2e504603a3cf97cbfd807d457a26c7
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2018-04-06 12:07:28 -0300

    schema being created sucessfully, data_file being stored successfully

--- code/sql/schema.sql
+++ code/sql/schema.sql
@@ -1,106 +1,115 @@
 CREATE SCHEMA waze;
 
-CREATE TABLE waze.signals
+CREATE TABLE waze.data_files
 (
-"id"                                BIGINT PRIMARY KEY NOT NULL,
-"startTimeMillis"                   BIGINT UNIQUE NOT NULL,
+"id"                                SERIAL PRIMARY KEY NOT NULL,
+"startTimeMillis"                   BIGINT NOT NULL,
 "endTimeMillis"                     BIGINT NOT NULL,
-"date_start"                        TIMESTAMP WITH TIME ZONE,
-"date_end"                          TIMESTAMP WITH TIME ZONE
+"startTime"                         TIMESTAMP,
+"endTime"                           TIMESTAMP,
+"date_created"                      TIMESTAMP,
+"date_updated"                      TIMESTAMP,
+"file_name"                         TEXT NOT NULL,
+"json_hash"                         uuid NOT NULL,
+UNIQUE ("startTimeMillis", "json_hash")
 );
 
 CREATE TABLE waze.jams 
 (
-  "id"                              BIGINT PRIMARY KEY NOT NULL,
-  "uuid"                            VARCHAR[500] NOT NULL,
+  "id"                              SERIAL PRIMARY KEY NOT NULL,
+  "uuid"                            TEXT NOT NULL,
   "pub_millis"                      BIGINT NOT NULL,
-  "start_node"                      VARCHAR[500],
-  "end_node"                        VARCHAR[500],
+  "pub_utc_date"                    TIMESTAMP,
+  "start_node"                      TEXT,
+  "end_node"                        TEXT,
   "road_type"                       INTEGER,
-  "street"                          VARCHAR[500],
-  "city"                            VARCHAR[500],
-  "country"                         VARCHAR[500],
+  "street"                          TEXT,
+  "city"                            TEXT,
+  "country"                         TEXT,
   "delay"                           INTEGER,
   "speed"                           float4,
   "length"                          INTEGER,
-  "turn_type"                       VARCHAR[500],
+  "turn_type"                       TEXT,
   "level"                           INTEGER,
-  "blocking_alert_id"               VARCHAR[500],
+  "blocking_alert_id"               TEXT,
   "coordinates"                     JSON,
-  "signal_id"                       BIGINT NOT NULL REFERENCES waze.signals (id)
+  "datafile_id"                     BIGINT NOT NULL REFERENCES waze.data_files (id)
 );
 
 CREATE TABLE waze.alerts 
 (
-  "id"                            BIGINT PRIMARY KEY NOT NULL,
-  "uuid"                          VARCHAR[500] NOT NULL, 
-  "pub_millis"                    BIGINT NOT NULL,
-  "road_type"                     INTEGER,
-  "street"                        VARCHAR[500],
-  "city"                          VARCHAR[500],
-  "country"                       VARCHAR[500],
-  "magvar"                        INTEGER,
-  "reliability"                   INTEGER,
-  "report_description"            VARCHAR[500],
-  "report_rating"                 INTEGER,
-  "type"                          VARCHAR[500],
-  "subtype"                       VARCHAR[500],
-  "report_by_municipality_user"   BOOLEAN,
-  "thumbs_up"                     INTEGER,
-  "signal_id"                     BIGINT NOT NULL REFERENCES waze.signals (id)
+  "id"                              SERIAL PRIMARY KEY NOT NULL,
+  "uuid"                            TEXT NOT NULL, 
+  "pub_millis"                      BIGINT NOT NULL,
+  "pub_utc_date"                    TIMESTAMP,
+  "road_type"                       INTEGER,
+  "street"                          TEXT,
+  "city"                            TEXT,
+  "country"                         TEXT,
+  "magvar"                          INTEGER,
+  "reliability"                     INTEGER,
+  "report_description"              TEXT,
+  "report_rating"                   INTEGER,
+  "type"                            TEXT,
+  "subtype"                         TEXT,
+  "report_by_municipality_user"     BOOLEAN,
+  "thumbs_up"                       INTEGER,
+  "jam_id"                          TEXT,
+  "irregularity_id"                 TEXT,
+  "datafile_id"                     BIGINT NOT NULL REFERENCES waze.data_files (id)
 );
 
 CREATE TABLE waze.irregularities 
 (
-  "id"                      BIGINT PRIMARY KEY NOT NULL,
-  "uuid"                    VARCHAR[500] NOT NULL,
-  "detection_date_millis"   BIGINT NOT NULL,
-  "detection_date"          TIMESTAMP,
-  "update_date_millis"      BIGINT NOT NULL,
-  "update_date"             TIMESTAMP,
-  "street"                  VARCHAR[500],
-  "city"                    VARCHAR[500],
-  "country"                 VARCHAR[500],
-  "is_highway"              BOOLEAN,
-  "speed"                   float4,
-  "regular_speed"           float4,
-  "delay_seconds"           INTEGER,
-  "seconds"                 INTEGER,
-  "length"                  INTEGER,
-  "trend"                   INTEGER,
-  "type"                    VARCHAR[500],
-  "severity"                float4,
-  "jam_level"               INTEGER,
-  "drivers_count"           INTEGER,
-  "alerts_count"            INTEGER,
-  "n_thumbs_up"             INTEGER,
-  "n_comments"              INTEGER,
-  "n_images"                INTEGER,
-  "signal_id"               BIGINT NOT NULL REFERENCES waze.signals (id)
+  "id"                              SERIAL PRIMARY KEY NOT NULL,
+  "uuid"                            TEXT NOT NULL,
+  "detection_date_millis"           BIGINT NOT NULL,
+  "detection_date"                  TIMESTAMP,
+  "update_date_millis"              BIGINT NOT NULL,
+  "update_date"                     TIMESTAMP,
+  "street"                          TEXT,
+  "city"                            TEXT,
+  "country"                         TEXT,
+  "is_highway"                      BOOLEAN,
+  "speed"                           float4,
+  "regular_speed"                   float4,
+  "delay_seconds"                   INTEGER,
+  "seconds"                         INTEGER,
+  "length"                          INTEGER,
+  "trend"                           INTEGER,
+  "type"                            TEXT,
+  "severity"                        float4,
+  "jam_level"                       INTEGER,
+  "drivers_count"                   INTEGER,
+  "alerts_count"                    INTEGER,
+  "n_thumbs_up"                     INTEGER,
+  "n_comments"                      INTEGER,
+  "n_images"                        INTEGER,
+  "datafile_id"                     BIGINT NOT NULL REFERENCES waze.data_files (id)
 );
 
 
 CREATE TABLE waze.coordinates 
 (
-  "id"                BIGINT PRIMARY KEY NOT NULL,
-  "latitude"          float8 NOT NULL,
-  "longitude"         float8 NOT NULL,
-  "order"             INTEGER NOT NULL,
-  "jam_id"            BIGINT REFERENCES waze.jams (id),
-  "irregularity_id"   BIGINT REFERENCES waze.irregularities (id),
-  "alert_id"          BIGINT REFERENCES waze.alerts (id)
+  "id"                              SERIAL PRIMARY KEY NOT NULL,
+  "latitude"                        float8 NOT NULL,
+  "longitude"                       float8 NOT NULL,
+  "order"                           INTEGER NOT NULL,
+  "jam_id"                          BIGINT REFERENCES waze.jams (id),
+  "irregularity_id"                 BIGINT REFERENCES waze.irregularities (id),
+  "alert_id"                        BIGINT REFERENCES waze.alerts (id)
 );
 
 CREATE TABLE waze.roads 
 (
-  "id"      INTEGER PRIMARY KEY NOT NULL,
-  "value"   INTEGER NOT NULL,
-  "name"    VARCHAR[100] NOT NULL
+  "id"                              SERIAL PRIMARY KEY NOT NULL,
+  "value"                           INTEGER NOT NULL,
+  "name"                            VARCHAR[100] NOT NULL
 );
 
 CREATE TABLE waze.alert_types 
 (
-  "id"        BIGINT PRIMARY KEY NOT NULL,
-  "type"      VARCHAR[500] NOT NULL,
-  "subtype"   VARCHAR[500]
+  "id"                              SERIAL PRIMARY KEY NOT NULL,
+  "type"                            TEXT NOT NULL,
+  "subtype"                         TEXT
 );
