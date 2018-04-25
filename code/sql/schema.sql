a380d9f67bfaf4b926b758767353872af9e902a9
Michael Schnuerle <github@purplesquirrel.net>
2018-04-25 13:19:55 -0400

    Added roads and alert type data
    And fixed a varchar typo in roads table.

--- code/sql/schema.sql
+++ code/sql/schema.sql
@@ -135,7 +135,7 @@ CREATE TABLE waze.roads
 (
   "id"                              SERIAL PRIMARY KEY NOT NULL,
   "value"                           INTEGER NOT NULL,
-  "name"                            VARCHAR[100] NOT NULL
+  "name"                            VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE waze.alert_types 
@@ -149,3 +149,67 @@ CREATE TABLE waze.alert_types
 INSERT INTO waze.coordinate_type (id, type_name) VALUES (1, 'Line');
 INSERT INTO waze.coordinate_type (id, type_name) VALUES (2, 'Turn Line');
 INSERT INTO waze.coordinate_type (id, type_name) VALUES (3, 'Location');
+
+-- load roads
+INSERT INTO waze.roads (value, name) VALUES (1, 'Streets');
+INSERT INTO waze.roads (value, name) VALUES (2, 'Primary Street');
+INSERT INTO waze.roads (value, name) VALUES (3, 'Freeways');
+INSERT INTO waze.roads (value, name) VALUES (4, 'Ramps');
+INSERT INTO waze.roads (value, name) VALUES (5, 'Trails');
+INSERT INTO waze.roads (value, name) VALUES (6, 'Primary');
+INSERT INTO waze.roads (value, name) VALUES (7, 'Secondary');
+INSERT INTO waze.roads (value, name) VALUES (8, '4X4 Trails');
+INSERT INTO waze.roads (value, name) VALUES (9, 'Walkway');
+INSERT INTO waze.roads (value, name) VALUES (10, 'Pedestrian');
+INSERT INTO waze.roads (value, name) VALUES (11, 'Exit');
+INSERT INTO waze.roads (value, name) VALUES (12, '?');
+INSERT INTO waze.roads (value, name) VALUES (13, '?');
+INSERT INTO waze.roads (value, name) VALUES (14, '4X4 Trails');
+INSERT INTO waze.roads (value, name) VALUES (15, 'Ferry crossing');
+INSERT INTO waze.roads (value, name) VALUES (16, 'Stairway');
+INSERT INTO waze.roads (value, name) VALUES (17, 'Private road');
+INSERT INTO waze.roads (value, name) VALUES (18, 'Railroads');
+INSERT INTO waze.roads (value, name) VALUES (19, 'Runway/Taxiway');
+INSERT INTO waze.roads (value, name) VALUES (20, 'Parking lot road');
+INSERT INTO waze.roads (value, name) VALUES (21, 'Service road');
+
+-- load alert_types
+INSERT INTO waze.alert_types (type, subtype) VALUES ('ACCIDENT', 'ACCIDENT_MINOR');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('ACCIDENT', 'ACCIDENT_MAJOR');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('ACCIDENT', 'NO_SUBTYPE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('JAM', 'JAM_MODERATE_TRAFFIC');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('JAM', 'JAM_HEAVY_TRAFFIC');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('JAM', 'JAM_STAND_STILL_TRAFFIC');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('JAM', 'JAM_LIGHT_TRAFFIC');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('JAM', 'NO_SUBTYPE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_SHOULDER');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_OBJECT');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_POT_HOLE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_ROAD_KILL');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_SHOULDER_CAR_STOPPED');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_SHOULDER_ANIMALS');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_SHOULDER_MISSING_SIGN');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_FOG');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_HAIL');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_HEAVY_RAIN');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_HEAVY_SNOW');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_FLOOD');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_MONSOON');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_TORNADO');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_HEAT_WAVE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_HURRICANE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_WEATHER_FREEZING_RAIN');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_LANE_CLOSED');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_OIL');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_ICE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_CONSTRUCTION');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_CAR_STOPPED');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'HAZARD_ON_ROAD_TRAFFIC_LIGHT_FAULT');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('WEATHERHAZARD/HAZARD', 'NO_SUBTYPE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('MISC', 'NO_SUBTYPE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('CONSTRUCTION', 'NO_SUBTYPE');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('ROAD_CLOSED', 'ROAD_CLOSED_HAZARD');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('ROAD_CLOSED', 'ROAD_CLOSED_CONSTRUCTION');
+INSERT INTO waze.alert_types (type, subtype) VALUES ('ROAD_CLOSED', 'ROAD_CLOSED_EVENT');
