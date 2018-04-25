4be7bf8c0e3a3d91f4165051ad973f4a19e8eea1
Jason Still <jason@ignew.com>
2018-04-25 09:42:14 -0400

    Add production environment to terraform
    Added env-prod environment folder/files, with some slightly different settings from dev

---
+++ infrastructure/terraform/environment/env-prod/environment.tf
@@ -0,0 +1,45 @@
+# setup the s3 backend for storing state
+# see config file in backend folder for further required settings
+terraform {
+    backend "s3" {
+        # name of the state file, unique per environment; could instead tweak it and use workspaces
+        key = "env-prod-waze-data-processor.tfstate" 
+    }
+}
+
+###############################################
+
+module "globals" {
+    source = "../../modules/globals"
+    environment = "Production"
+}
+
+# load the environment module, passing variables when available from the globals output (see globals module readme for the 'why')
+module "environment" {
+    source = "../../modules/environment"
+    environment = "${module.globals.environment}"
+    object_name_prefix = "${module.globals.object_name_prefix}"
+
+    # consider whether you want these on or not and the potential cost impact
+    enable_data_processor_dlq_sns_topic = "true"
+    enable_data_processed_sns_topic = "true"
+
+    # since this is production, we won't allow deleting the db unless a final snapshot has been taken
+    # also won't allow deleting the buckets if they still have files in them
+    # this means destroying the stack requires manual intervention outside of terraform, so no one accidentally loses production data
+    skip_final_db_snapshot_on_destroy = "false"
+    empty_s3_buckets_before_destroy = "false"
+
+    default_resource_region = "${module.globals.default_resource_region}" # see globals.tf to change
+    waze_data_url = "${module.globals.waze_data_url}" # see globals.tf to change
+    rds_master_username = "${module.globals.rds_master_username}" # see globals.tf to change
+    rds_master_password = "${module.globals.rds_master_password}" # see globals.tf to change
+    lambda_db_username = "${module.globals.lambda_db_username}" # see globals.tf to change
+    lambda_db_password = "${module.globals.lambda_db_password}" # see globals.tf to change
+    
+    # Remove the comment from this variable to set your own CIDR block range to avoid conflicts
+    # rds_vpc_cidr_block = "10.20.0.0/16"
+
+    # if you're not running this in the local directory, you'll likely need to update this value
+    lambda_artifacts_path = "../../../../code/lambda-functions"
+}
