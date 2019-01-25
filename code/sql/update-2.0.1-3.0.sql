1dcc2027529bd2d7dfb9b9bd7ad387898575ed2f
Michael Schnuerle <github@purplesquirrel.net>
2019-01-25 11:28:23 -0500

    Merging some difficult code manually

--- code/sql/update-2.0.1-3.0.sql
+++ code/sql/update-2.0.1-3.0.sql
@@ -14,11 +14,6 @@ WHERE a.type = t.type
  and a.subtype = t.subtype 
  and a.type_id is null; 
  
--- add Jams fields
-ALTER TABLE waze.jams ADD ns_direction varchar(3) ;
-ALTER TABLE waze.jams ADD ew_direction varchar(3) ;
-ALTER TABLE waze.jams ADD dayofweek int4 ;
-
 -- need to add type_id data upon ingestion...
 
 -- Create read-only user
@@ -31,3 +26,27 @@ GRANT USAGE ON SCHEMA waze TO waze_readonly;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA waze TO waze_readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA waze to waze_readonly;
 ALTER DEFAULT PRIVILEGES IN SCHEMA waze GRANT SELECT ON TABLES TO waze_readonly;
+
+
+-- version table will be used for version tracking and upgrading
+-- THIS TABLE MUST GET A RECORD ADDED WITH NEW INSTALLS TO INDICATE WHICH VERSION GOT INSTALLED
+-- Update scripts MUST ALSO add a new row to the table to indicate the new version
+CREATE TABLE waze.application_version
+(
+  "version_number"                  VARCHAR(30) PRIMARY KEY NOT NULL,
+  "install_date"                    TIMESTAMP NOT NULL
+)
+
+-- insert the CURRENT VERSION NUMBER into the version table
+INSERT INTO waze.application_version (version_number, install_date) VALUES ('2.1', current_timestamp);
+
+
+-- add Jams fields
+ALTER TABLE waze.jams ADD ns_direction varchar(3) ;
+ALTER TABLE waze.jams ADD ew_direction varchar(3) ;
+ALTER TABLE waze.jams ADD dayofweek int4 ;
+ALTER TABLE waze.jams ADD geom_line geography(LINESTRING) ;
+
+-- add Alerts fields
+ALTER TABLE waze.alerts ADD dayofweek INTEGER ;
+ALTER TABLE waze.alerts ADD geom_point geography(POINT) ;
